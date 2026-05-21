448249635802643135d553fddde3b96ec4327542
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2026-05-21 14:50:13 +0200

    feature: implement ProductService with CRUD operations and ProductMapper integration

--- src/main/java/se/iths/productservicegroup2/controller/ProductController.java
+++ src/main/java/se/iths/productservicegroup2/controller/ProductController.java
@@ -1,7 +1,30 @@
 package se.iths.productservicegroup2.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.iths.productservicegroup2.dto.ProductResponse;
+import se.iths.productservicegroup2.service.ProductService;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAll() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
 }

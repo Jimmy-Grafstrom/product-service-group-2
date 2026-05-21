9782fc7a568083f9a4f98057ea09b3bb57574bc4
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2026-05-21 13:11:01 +0200

    fix: add validation dependency and correct exception constructor

--- src/main/java/se/iths/productservicegroup2/exceptions/ProductNotFoundException.java
+++ src/main/java/se/iths/productservicegroup2/exceptions/ProductNotFoundException.java
@@ -1,13 +1,11 @@
 package se.iths.productservicegroup2.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
-    @ExceptionHandler(ProductNotFoundException.class)
-    public String handleProductNotFound(ProductNotFoundException ex) {
-        return ex.getMessage();
+    public ProductNotFoundException(String message) {
+        super(message);
     }
 }

77ebfc7130df8cb7142a27b97aaefcd1aabbd2ae
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2026-05-21 09:52:16 +0200

    feature: enhance Product entity with additional fields and Lombok annotations

--- src/main/java/se/iths/productservicegroup2/model/Product.java
+++ src/main/java/se/iths/productservicegroup2/model/Product.java
@@ -1,14 +1,29 @@
 package se.iths.productservicegroup2.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
 
 @Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+    @Column(name = "description", nullable = false, length = 500)
+    private String description;
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+    @Column(name = "stock", nullable = false)
+    private int stock = 0;
 }

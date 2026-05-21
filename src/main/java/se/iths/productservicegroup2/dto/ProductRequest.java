c887354d1c22691bddd4b93476ad028017f30140
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2026-05-21 10:26:39 +0200

    feature: add validation annotations to ProductRequest and update ProductResponse structure

--- src/main/java/se/iths/productservicegroup2/dto/ProductRequest.java
+++ src/main/java/se/iths/productservicegroup2/dto/ProductRequest.java
@@ -1,4 +1,20 @@
 package se.iths.productservicegroup2.dto;
 
-public record ProductRequest() {
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(
+        @NotBlank(message = "Name must not be blank")
+        @Size(max = 100, message = "Name must not exceed 100 characters")
+        String name,
+        @NotBlank
+        @Size(max = 500, message = "Description must not exceed 500 characters")
+        String description,
+        @NotNull(message = "Price must not be null")
+        @Positive(message = "Price must be positive")
+        BigDecimal price,
+        @Min(value = 0, message = "Stock must be zero or positive")
+        int stock
+) {
 }

daa5b60933d5b5756d8569b097a8cce07f44cb9b
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2026-05-24 12:00:54 +0200

    feature: remove unused import in ProductMapper to clean up code

--- src/main/java/se/iths/productservicegroup2/mapper/ProductMapper.java
+++ src/main/java/se/iths/productservicegroup2/mapper/ProductMapper.java
@@ -1,7 +1,6 @@
 package se.iths.productservicegroup2.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import se.iths.productservicegroup2.dto.ProductRequest;
 import se.iths.productservicegroup2.dto.ProductResponse;
 import se.iths.productservicegroup2.model.Product;

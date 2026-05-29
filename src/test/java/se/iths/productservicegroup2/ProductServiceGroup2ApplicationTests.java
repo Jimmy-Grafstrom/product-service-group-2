1c12c1cc8ae251c415fd1b7977b43fe107d30ad6
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2026-05-29 11:12:00 +0200

    feature: configure JWT issuer URI for testing in ProductServiceGroup2ApplicationTests

--- src/test/java/se/iths/productservicegroup2/ProductServiceGroup2ApplicationTests.java
+++ src/test/java/se/iths/productservicegroup2/ProductServiceGroup2ApplicationTests.java
@@ -3,7 +3,9 @@ package se.iths.productservicegroup2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:8080/realms/dummy"
+})
 class ProductServiceGroup2ApplicationTests {
 
     @Test

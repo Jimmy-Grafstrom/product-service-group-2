60e84c4b9ef521e7f694f52f3ae561d0a4cca51a
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2026-06-03 16:41:08 +0200

    refactor: clean up comments and remove unused mapping in ProductMapper

--- src/main/java/se/iths/productservicegroup2/service/ProductService.java
+++ src/main/java/se/iths/productservicegroup2/service/ProductService.java
@@ -54,33 +54,33 @@ public class ProductService {
 
     @Transactional
     public List<ProductInfo> decreaseStock(List<ProductStockRequest> requestedProducts) {
-        // Find all id from db
+
         List<Long> idList = requestedProducts.stream()
                 .map(ProductStockRequest::id)
                 .toList();
-        // One DB request, save all products to list
+
         List<Product> products = productRepository.findAllById(idList);
-        // Check if length matches
+
         if (products.size() != requestedProducts.size()) {
             throw new ProductNotFoundException("Requested " + requestedProducts.size() +
                     " products, but only found " + products.size() + " in database.");
         }
-        // Empty list with responses
+
         List<ProductInfo> responseList = new ArrayList<>();
-        // Loop through requested products
+
         for (ProductStockRequest request : requestedProducts) {
-            // Compare ids from db and requested products. Throw exception if the ids don't match.
+
             Product product = products.stream()
                     .filter(p -> p.getId().equals(request.id()))
                     .findFirst()
                     .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + request.id()));
-            // Check stock
+
             if (request.quantity() > product.getStock()) {
                 throw new InsufficientStockException("Insufficient stock for product: " + product.getName());
             }
-            // Update stock
+
             product.setStock(product.getStock() - request.quantity());
-            // Create response, Transactional automatically saves the updates to database
+
             responseList.add(mapper.toInfo(product, request.quantity()));
         }
         return responseList;
